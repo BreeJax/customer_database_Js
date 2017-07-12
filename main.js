fa5c03dd81139e9ac20205890b13e1b2fc1253f9
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-07-12 19:12:09 -0400

    attampting hard mode here soon

--- main.js
+++ main.js
@@ -10,15 +10,30 @@ for (var i = 0; i < customers.results.length; i++) {
   nameElement.innerHTML = `${name.first} ${name.last} `
   _li.appendChild(nameElement)
 
+  let email = customers.results[i].email
+  let emailElement = document.createElement("p")
+  emailElement.innerHTML = email
+  _li.appendChild(emailElement)
+
   let location = customers.results[i].location
   let locationElement = document.createElement("h7")
-  locationElement.innerHTML = `${location.street} ${location.city}`
+  locationElement.innerHTML = `${location.street}`
   _li.appendChild(locationElement)
 
   let location2 = customers.results[i].location
   let location2Element = document.createElement("h7")
-  location2Element.innerHTML = `${location2.state} ${location2.postcode}`
+  location2Element.innerHTML = `${location.city} ${location2.state} ${location2.postcode}`
   _li.appendChild(location2Element)
+
+  let phone = customers.results[i].phone
+  let phoneElement = document.createElement("p")
+  phoneElement.innerHTML = phone
+  _li.appendChild(phoneElement)
+
+  let id = customers.results[i].id
+  let idElement = document.createElement("h4")
+  idElement.innerHTML = `${id.value}`
+  _li.appendChild(idElement)
 
   document.querySelector("ul").appendChild(_li)
 }
